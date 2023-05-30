c3a177f90ffcbaa610aaa3ab8c296c9f586e6156
root <root@ip-172-31-93-83.ec2.internal>
2023-05-30 04:16:22 +0000

    wip

--- devops/hooks/after-install.sh
+++ devops/hooks/after-install.sh
@@ -7,11 +7,18 @@ export WEB_USER="ec2-user"
 
 cd $WEB_DIR
 
+#creating .env file
+cp .env.example .env
+
+DB_USERNAME=$(aws ssm get-parameter --name /database/prd/username --with-decryption --query Parameter.Value)
+sed -i 's/##DB_USERNAME##/$DB_USERNAME/g' /tmp/.env
+
 # change user owner to apach
 sudo chown -R apache:apache .
 
 # install composer deps
 sudo -u $WEB_USER composer install --no-dev --no-progress --prefer-dist
+
 
 # generate app key
 sudo -u $WEB_USER php artisan key:generate
